845a8e829327e5a992e772524ae7c587072f702d
Pandey <kispande@38f9d3647b46.ant.amazon.com>
2019-03-26 16:15:52 -0700

    first try

---
+++ js/app.js
@@ -0,0 +1,39 @@
+"use strict";
+do {
+  var answer = prompt("My name is Tom (Yes/No)?");
+  answer = answer.toLowerCase();
+}while(answer !="y"&& answer!="n"&&answer!="yes"&&answer!="no");
+
+
+if (answer ==="yes"||answer==="y") {
+ 
+alert("we have the same name");
+
+}else 
+{
+  alert("Welcome to the team");
+ 
+}
+console.log( "what is my name?");
+// var question =prompt("Am I from Washington? (YES/NO)")
+// question = question.toUpperCase();
+// if (question ==="YES" || question ==="Y") {
+
+//   alert("Welcome Home");
+// } else
+// {
+//   alert("Where are you from?");
+  
+// }
+//   console.log("where am I from");
+
+//   if (question ==="YES" || question ==="Y") {
+
+//   alert("Welcome Home");
+// } else
+// {
+//   alert("Where are you from?");
+  
+// }
+//   console.log("where am I from");
+
