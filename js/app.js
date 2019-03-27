e0fc24433df4d15a289e55bc3d2e0d66ea9cd210
Pandey <kispande@38f9d3647b46.ant.amazon.com>
2019-03-27 16:28:38 -0700

    Lab 2 and 3

--- js/app.js
+++ js/app.js
@@ -1,81 +1,94 @@
-"use strict";
+'use strict';
 do {
-  var answer = prompt("My name is Tom (Yes/No)?");
+  var answer = prompt('My name is Tom (Yes/No)?');
   answer = answer.toLowerCase();
-}while(answer !='y'&& answer!='n'&&answer!='yes'&&answer!='no');
+}while(answer !=='y'&& answer!=='n'&&answer!=='yes'&&answer!=='no');
 
 
 if (answer ==='yes'||answer==='y') {
- 
-alert('we have the same name');
-
-}else 
-{
-  alert('Welcome to the game');
- 
-}
+  alert('we have the same name');}
+else {alert('Welcome to the game');}
 console.log( 'what is my name?');
 do {
-  var answer = prompt('I live in Seattle (Yes/No)?');
+  answer = prompt('I live in Seattle (Yes/No)?');
   answer = answer.toLowerCase();
-}while(answer !='y'&& answer!='n'&&answer!='yes'&&answer!='no');
+}while(answer !=='y'&& answer!=='n'&&answer!=='yes'&&answer!=='no');
 
 
 if (answer ==='yes'||answer==='y') {
- 
-alert('we live in different place');
+  alert('we live in different place');
 
-}else 
-{
+}else {
   alert('I might know where that is');
- 
 }
 console.log( 'where do I live?');
 do {
-  var answer = prompt('I eat sandwich(Yes/No)?');
+  answer = prompt('I eat sandwich(Yes/No)?');
   answer = answer.toLowerCase();
-}while(answer !='y'&& answer!='n'&&answer!='yes'&&answer!='no');
+}while(answer !=='y'&& answer!=='n'&&answer!=='yes'&&answer!=='no');
 
 
 if (answer ==='yes'||answer==='y') {
- 
-alert('That is right');
-
-}else 
-{
+  alert('That is right');} else {
   alert('Good guess');
- 
 }
 console.log( 'what do I eat?');
 do {
-  var answer = prompt('I study in Seattle (Yes/No)?');
+  answer = prompt('I study in Seattle (Yes/No)?');
   answer = answer.toLowerCase();
-}while(answer !='y'&& answer!='n'&&answer!='yes'&&answer!='no');
+}while(answer !=='y'&& answer!=='n'&&answer!=='yes'&&answer!=='no');
 
 
 if (answer ==='yes'||answer==='y') {
- 
-alert('Its a nice place to study');
-
-}else 
-{
+  alert('Its a nice place to study');
+}else {
   alert('which city do I study?');
- 
 }
 console.log( 'where do I study?');
 do {
-  var answer = prompt('I commute by train (Yes/No)?');
+  answer = prompt('I commute by train (Yes/No)?');
   answer = answer.toLowerCase();
-}while(answer !='y'&& answer!='n'&&answer!='yes'&&answer!='no');
+}while(answer !=='y'&& answer!=='n'&&answer!=='yes'&&answer!=='no');
 
 
 if (answer ==='yes'||answer==='y') {
- 
-alert('Thats a nice way to travel');
-
-}else 
-{
-  alert("try travellin train");
- 
+  alert('Thats a nice way to travel');
+}else {
+  alert('try travellin train');
 }
-console.log( "how do I travel?");
+console.log( 'how do I travel?');
+var questionArray = [ 'whats my lucky number', 'what was my score on a quiz','how many miles do I travel everyday?', 'what is the passing score?'];
+var count = 2;
+var isCorrect =0;
+alert('now lets play first guessing game! You get few tries');
+do {
+  var userNumber = prompt(questionArray[0]);
+  if (userNumber > 50){
+    alert( 'Aim high!!');
+  }
+  else if (userNumber < 50){
+    alert('Too low');
+  }
+  else {
+    alert('Thats right!!');
+    isCorrect = 1;
+  }
+  count--;
+}while(count !== 0 && isCorrect ===0);
+alert(' now lets play aother guessing game! You get many tries');
+do {
+  userNumber = prompt(questionArray[3]);
+  if (userNumber > 70){
+    alert( 'Keep it up');
+  }
+  else if (userNumber < 70){
+    alert('Study Hard');
+  }
+  else {
+    alert('You Passed!!');
+    isCorrect = 1;
+  }
+  count--;
+}while(count !== 0 && isCorrect ===0);
+
+
